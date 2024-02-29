e6e6bfc4471d4f96a76c96e47847e1b709b8d5b4
ShivamSh002 <shivamsharma98879@gmail.com>
2024-02-29 16:11:31 +0530

    commit

--- x-pagination/src/App.js
+++ x-pagination/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      );
+      setData(response.data);
+    } catch (error) {
+      setError("Failed to fetch data");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const itemsPerPage = 10;
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+
+  const handleClickPrevious = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleClickNext = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const renderData = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = Math.min(startIndex + itemsPerPage, data.length);
+    const slicedData = data.slice(startIndex, endIndex);
+
+    return slicedData.map((entry, index) => (
+      <tr key={index}>
+        <td> {entry.id} </td> <td> {entry.name} </td> <td> {entry.email} </td>{" "}
+        <td> {entry.role} </td>{" "}
+      </tr>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      {" "}
+      {error && <div> Error: {error} </div>}{" "}
+      <div className="head">
+        {" "}
+        <h1> Employee Data Table </h1>{" "}
+      </div>{" "}
+      <div className="table">
+        {" "}
+        <table>
+          <thead>
+            <tr>
+              <th> ID </th> <th> Username </th> <th> Email </th> <th> Role </th>{" "}
+            </tr>{" "}
+          </thead>{" "}
+          <tbody> {renderData()} </tbody>{" "}
+        </table>{" "}
+      </div>{" "}
+      <div className="btn">
+        <button onClick={handleClickPrevious}> Previous </button>{" "}
+        <button> {currentPage} </button>{" "}
+        <button onClick={handleClickNext}> Next </button>{" "}
+      </div>{" "}
     </div>
   );
-}
+};
 
 export default App;
